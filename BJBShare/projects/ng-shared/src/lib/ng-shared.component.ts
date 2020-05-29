6362b61c1cfd5c3d4680a240bed9f514371b52a4
Tharnid <jeffmsmithdev@gmail.com>
2020-05-29 18:20:05 -0400

    project created

---
+++ BJBShare/projects/ng-shared/src/lib/ng-shared.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-ng-shared',
+  template: `
+    <p>
+      ng-shared works!
+    </p>
+  `,
+  styles: []
+})
+export class NgSharedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- BJBShare/src/app/app.module.ts
+++ BJBShare/src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedComponent } from './shared/shared.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      SharedComponent,
+      PostCreateComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      BrowserAnimationsModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
